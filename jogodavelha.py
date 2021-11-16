fec9a411509d9247a877ffd9d320b7a0cbc20497
Gabryel Alexandre Campos da Silva <gabryel2348369jg@gmail.com>
2021-11-16 02:32:16 -0300

    Logica Player Humano e CPU

--- jogodavelha.py
+++ jogodavelha.py
@@ -14,7 +14,7 @@ jogarDenovo = True
 quantidadeDeJogadas = 0
 quantidadeMaximaDeJogadas = 9 
 jogadorVez = 2
-vit = False
+vit = "n"
 matriz = [
           [" "," "," "],
           [" "," "," "],
@@ -36,4 +36,134 @@ def tela():
   print("Jogadas: "+str(quantidadeDeJogadas))
 
 
+def jogadorHumano():
+  global quantidadeDeJogadas
+  global jogadorVez
+  global quantidadeMaximaDeJogadas
+  global matriz
+
+  if jogadorVez == 2 and quantidadeDeJogadas < quantidadeMaximaDeJogadas:
+    
+
+    try:
+      linha = int (input("Digite a Linha : "))
+      coluna = int (input("Digite a coluna : "))
+      while matriz[linha][coluna] !=" ":
+        linha = int (input("Digite a Linha : "))
+        coluna = int (input("Digite a coluna : "))
+      matriz [linha][coluna] = "X"
+      jogadorVez = 1
+      quantidadeDeJogadas+= 1
+    except:
+      print("Linha ou coluna inválida")
+      os.system("pause")
+
+
+def jogadorCPU():
+  global quantidadeDeJogadas
+  global jogadorVez
+  global quantidadeMaximaDeJogadas
+  global matriz
+  
+  if jogadorVez == 1 and quantidadeDeJogadas < quantidadeMaximaDeJogadas:
+    
+    linha =  random.randrange(0,3)
+    coluna = random.randrange(0,3)
+    while matriz[linha][coluna] !=" ":
+      linha = random.randrange(0,3)
+      coluna = random.randrange(0,3)
+    matriz[linha][coluna]="O"
+    quantidadeDeJogadas+=1
+    jogadorVez = 2
+
+def verificador():
+  global matriz
+  vitoria = "n"
+  simbolos = ["X","O"]
+
+  for s in simbolos:
+    vitoria = "n"
+    ic=il = 0
+    #Verificar Linhas
+    while il <3:
+      soma = 0
+      ic = 0 
+      while ic < 3:
+        if(matriz[il][ic]==s):
+          soma+=1
+        ic+=1
+      
+      if(soma==3):
+        vitoria = s
+        break
+      il+=1
+    if(vitoria!="n"):
+      break
+    #Verificar Colunas
+    while ic <3:
+      soma = 0
+      il = 0 
+      while il < 3:
+        if(matriz[il][ic]==s):
+          soma+=1
+        il+=1
+      
+      ic+=1
+      if(soma==3):
+        vitoria = s
+        break
+    if(vitoria!="n"):
+      break
+
+    #Verificar Diagonal 1
+    soma = 0 
+    idiag = 0
+    while(idiag < 3 ):
+      if(matriz[idiag][idiag]==s):
+        soma+=1
+
+      idiag+=1
+    if(soma==3):
+      vitoria = s
+      break
+     #Verificar Diagonal 2
+    soma = 0 
+    idiagl = 0
+    idiagc = 2
+    while(idiagc >= 0 ):
+      if(matriz[idiagl][idiagc]==s):
+        soma+=1
+
+      idiagl+=1
+      idiagc-=1
+    if(soma==3):
+      vitoria = s
+      break
+  return vitoria
+
+def redefinir():
+  global matriz 
+  global quantidadeDeJogadas
+  global quantidadeMaximaDeJogadas
+  global jogadorVez
+  global vit 
+  quantidadeDeJogadas = 0
+  quantidadeMaximaDeJogadas = 9 
+  jogadorVez = 2
+  vit = "n"
+  matriz = [
+          [" "," "," "],
+          [" "," "," "],
+          [" "," "," "]
+  ]
+
+
 while(True):
+  tela()
+  jogadorHumano()
+  jogadorCPU()
+  vit = verificador()
+  print(vit)
+  print(quantidadeDeJogadas)
+  if vit!="n" or quantidadeDeJogadas >=quantidadeMaximaDeJogadas:
+    break
